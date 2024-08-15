bcdf2e366cf5338d0c39ffe0d282c5acb4f9c49a
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2024-08-15 22:39:04 +0530

    chore: Add react-router-dom package and update App.js for routing

---
+++ src/persona/Persona.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Persona = () => {
+  return (
+    <div>persona</div>
+  )
+}
+
+export default Persona
---
+++ src/pixel-perfect/Pixel_perfect.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pixel_perfect = () => {
+  return (
+    <div>Pixel_perfect</div>
+  )
+}
+
+export default Pixel_perfect
